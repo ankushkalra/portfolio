08449846b2eefbc44cc76f821f598b37030d28c3
Ankush Kalra <ankushkalra2@gmail.com>
2023-03-08 04:44:17 +0530

    removed AwesomeColors and dp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import React from 'react';
-import dp from './images/dp2.jpeg';
 import Instagram from './images/Instagram-Svg.svg';
 import LinkedIn from './images/LinkedIn-Svg.svg';
 import Twitter from './images/Twitter-Svg.svg';
 import Youtube from './images/Youtube-Svg.svg';
 import './App.css';
 import Theme from './Theme';
-import AwesomeColors from './components/AwesomeColors';
 
 function App() {
   return (
     <Theme>
-      <div className="App">
+      <div className="App" style={{ overflow: 'hidden', height: '100vh' }}>
         <section className="App-header">
-          <img src={dp} className="App-logo" alt="logo" />
-          <p>I am a JavaScript Developer, with experience in writing Node.js and React.js</p>
+          <p>
+            I am a JavaScript Developer, with experience in writing Node.js and
+            React.js
+          </p>
           <div>
-            <a href="https://twitter.com/iamankushkalra" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/iamankushkalra"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img className="Social-icon" src={Twitter} alt="Twitter link" />
             </a>
             <img className="Social-icon" src={Youtube} alt="Youtube link" />
-            <a href="https://www.linkedin.com/in/ankush-kalra-284222125/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/ankush-kalra-284222125/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <img className="Social-icon" src={LinkedIn} alt="LinkedIn link" />
             </a>
             <img className="Social-icon" src={Instagram} alt="Instagram link" />
           </div>
-        </section>
-        <section id="colors-gradients">
-          <AwesomeColors colors={['#ff2562']} />
         </section>
       </div>
     </Theme>
